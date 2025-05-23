cd9c8dae84145e07d4378f63174c2af65baab754
jinhyun <jin71744@gmail.com>
2025-05-23 16:27:19 +0900

    주석 제거

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -1,21 +1,11 @@
-import React from "react";
 import PokemonList from "./PokemonList";
-import PokemonCard from "./PokemonCard";
 import PokemonDeck from "./PokemonDeck";
-import { useDeck } from "../context/DeckContext";
 
 const DashBoard = () => {
-  // const { deck, onAddToDeck, onDeleteToDeck } = useDeck();
-  // console.log(onAddToDeck);
-
   return (
     <div>
-      <PokemonDeck
-      // deck={deck} onDeleteToDeck={onDeleteToDeck}
-      />
-      <PokemonList
-      // onAddToDeck={onAddToDeck}
-      />
+      <PokemonDeck />
+      <PokemonList />
     </div>
   );
 };
--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,6 +1,4 @@
-import React from "react";
 import DashBoard from "../components/DashBoard";
-import POKEMON_DATA from "../data/pokemon";
 import styled from "styled-components";
 import { DeckProvider } from "../context/DeckContext";
 
@@ -23,36 +21,11 @@ const Container = styled.div`
 `;
 
 const Dex = () => {
-  // const [deck, setDeck] = useState([]);
-
-  // const handleAddToDeck = (pokemon) => {
-  //   if (deck.find((p) => p.id === pokemon.id)) {
-  //     alert(`이미 선택한 포캣몬입니다.`);
-  //     return;
-  //   }
-
-  //   if (deck.length >= 6) {
-  //     alert(`더 이상 선택할 수 없습니다. `);
-  //     return;
-  //   }
-
-  //   setDeck([...deck, pokemon]);
-  // };
-
-  // const handleDeleteToDeck = (pokemon) => {
-  //   setDeck(deck.filter((p) => p.id !== pokemon.id));
-  // };
-
   return (
     <DeckProvider>
       <PageWrapper>
         <Container>
-          <DashBoard
-          // pokemon={POKEMON_DATA}
-          // deck={deck}
-          // onAddToDeck={handleAddToDeck}
-          // onDeleteToDeck={handleDeleteToDeck}
-          />
+          <DashBoard />
         </Container>
       </PageWrapper>
     </DeckProvider>
